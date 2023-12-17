f41a1cfba9ff8cc133d20449dfde1c651a62e608
Rahul Raj <118946861+abhiyendru01@users.noreply.github.com>
2023-12-17 15:06:36 +0530

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -36,7 +36,7 @@ const HeroContent = () => {
             Hii,
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              I'm Rahul{" "}
+              I&apos;m Rahul{" "}
             </span>
             </span>
         </motion.div>
